9f3f1d05f66fd811a6d04564b982ddb62fea3293
awpathum <awpathum@gmail.com>
2023-06-18 23:19:59 +0530

    video 7 done

--- profile-service/src/main/java/com/pathumw/rentacar/profileservice/controller/ProfileController.java
+++ profile-service/src/main/java/com/pathumw/rentacar/profileservice/controller/ProfileController.java
@@ -5,6 +5,7 @@ import com.pathumw.rentacar.profileservice.service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class ProfileController {
     @Autowired
     CustomerService customerService;
+    @RequestMapping(value = "/profile", method = RequestMethod.POST)
     public Customer save(@RequestBody Customer customer){
         return customerService.save(customer);
     }
